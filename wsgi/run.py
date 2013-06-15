d86b6a86f9c479945cb36d67486257f151c244f2
wurdum <pavel.ritikov@gmail.com>
2013-06-15 16:37:37 +0300

    routes was refactored

--- wsgi/run.py
+++ wsgi/run.py
@@ -20,12 +20,13 @@ def redactions():
 @app.route('/redactions/update')
 def redactions_update():
     redas = scrapers.get_redactions()
+
     db.save_redas(redas)
 
     return redirect(url_for('redactions'))
 
 
-@app.route('/spellshop/all', defaults={'reda': 'all'}, methods=['GET'])
+@app.route('/spellshop', defaults={'reda': 'all'}, methods=['GET'])
 @app.route('/spellshop/<reda>', methods=['GET'])
 def spellshop(reda):
     shops = [scrapers.SpellShopScraper.SHOP_NAME]
@@ -35,19 +36,16 @@ def spellshop(reda):
     return render_template('spellshop.html', active_reda=reda, redas=redas, cards=cards)
 
 
-@app.route('/spellshop/update/all', defaults={'reda': 'all'}, methods=['GET'])
+@app.route('/spellshop/update', defaults={'reda': 'all'}, methods=['GET'])
 @app.route('/spellshop/update/<reda>', methods=['GET'])
 def spellshop_update(reda):
     redas = filter(lambda r: 'spellshop' in r.shops,
                    db.get_redas() if reda == 'all' else db.get_redas(name=reda))
-    print ', '.join([re.name for re in redas])
+
     for r in redas:
         cards = filter(lambda c: c.has_info and c.has_prices, scrapers.SpellShopScraper.get_cards(r))
         db.save_cards(cards, shops=[scrapers.SpellShopScraper.SHOP_NAME])
-        print len(cards), r.name
 
-    # scrapers.MagiccardsScraper.get_card('Kemba\'s Legion', 'mirrodin besieged')
-    # scrapers.MagiccardsScraper.get_card('kembas legion', 'mirrodin besieged')
     return redirect(url_for('spellshop', reda=reda))
 
 
