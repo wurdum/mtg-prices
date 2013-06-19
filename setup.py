bcc0e14901d65afa01926d03d9648418d47c406a
wurdum <pavel.ritikov@gmail.com>
2013-06-19 23:41:52 +0300

    setup file credentials was fixed

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='prices',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='prices app',
+      author='wurdum',
+      author_email='wurdum.my@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Flask>=0.7.2', 'beautifulsoup4>=4.2.0', 'pymongo>=2.5.1', 'eventlet'],
 )
